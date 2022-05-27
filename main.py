65e0213efa117f5f02f5ad3e67ea7081cb6b8f2b
Manglam Patel <patelmangalam21880@gmail.com>
2022-05-27 22:36:49 +0530

    fix some missing files and improve code moving ball
    Now balls are shoot by the help of mouse but still bountrys is not made so ball jumps out of the screen & also created velocity to move ball

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from tkinter import Button
 import pygame
+import math
+
 
 pygame.init()
 
@@ -13,10 +14,15 @@ ball1x = 52.0
 ball1y = 51.0
 initmousepossx = 0.0
 initmousepossy = 0.0
+initmouseposs = [initmousepossy,initmousepossx]
 mousepossx = 0.0
 mousepossy = 0.0
+mouseposs = [mousepossy,mousepossx]
 leftpress = False
-
+velocityx = 0.0
+velocityy = 0.0
+velocity1d = 0.0 
+velocitymulti = 0.0
 
 # loading textures
 main_window = pygame.display.set_mode((screensize))
@@ -28,17 +34,44 @@ ball1 = pygame.transform.scale(ball1,(20,20))
 #main game loop
 while windowup:
     main_window.blit(bg,(0.0,0.0))
-    main_window.blit(ball1,(ball1x , ball1y))
+    main_window.blit(ball1,(ball1y , ball1x))
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             windowup = False
+
         if event.type == pygame.MOUSEBUTTONDOWN:
+            
             if event.button == 1:
                 leftpress = True
+                initmousepossy,initmousepossx = pygame.mouse.get_pos()
+                initmouseposs = [initmousepossy,initmousepossx]
                 print ("left click")
+                
+                
+                
         if event.type == pygame.MOUSEBUTTONUP:
+            
             if event.button == 1:
+                
+                mousepossy,mousepossx = pygame.mouse.get_pos()
+                velocityx = (initmousepossx-mousepossx)/-100
+                velocityy = (initmousepossy-mousepossy)/-100
                 leftpress = False
+                velocity1d = math.sqrt(pow(abs(velocityx),2)+pow(abs(velocityy),2))
+    
+    if leftpress == True:
+        if velocitymulti < 5:
+            velocitymulti += .1
+    
+    if(velocity1d>0):
+        velocity1d -= 0.1
+        ball1y += (velocitymulti*(-velocityy))
+        ball1x += (velocitymulti*(-velocityx))
+        if velocity1d < 0:
+            velocitymulti = 0.0
+            
+    print(velocitymulti)
     pygame.display.flip()
     clock.tick(60)
 
